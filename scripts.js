42cc0ab30486cd65bf4afa0d97e9f0eb4078a9fd
Garrett Steed <steed.garrett@gmail.com>
2020-10-16 20:02:01 -0600

    started over after tutor session, go timer working and css set up

---
+++ scripts.js
@@ -0,0 +1,65 @@
+
+//Question arrays - set for choices and answers     
+var questions = [
+    {
+    title: "What US state has the most coast line?",
+    choices: ["Texas", "Alaska",  "Florida", "Hawaii",],
+    answer: "Alaska"
+    },
+{
+    title: "What is the longest river in the US?",
+    choices: ["Missouri", "Yukon", "Mississippi", "Ohio",],
+    answer: "Missouri"
+    },
+{
+    title: "What is the most populated city in the US?",
+    choices: ["Dallas", "Chicago","LA", "New York City",],
+    answer: "New York City"
+    },
+{
+    title: "What is the most populated state in the US?",
+    choices: ["Michigan", "California", "New York", "Texas",],
+    answer: "California"
+    },
+{
+    title: "What is the highest mountain peak in the US?",
+    choices: ["Denali", "Mt. Whitney", "Mt. Rainier", "Gannett Peak",],
+    answer: "Denali"
+    },
+{
+    title: "What is the largest wildfire in US history?",
+    choices: ["250,000 acres", "150,000 acres", "1.2 million acres", "3 million acres",],
+    answer: "3 million acres"
+    },
+
+]
+
+//Numeric variables for timer and  
+var score = 0;
+var currentQuestion = -1;
+var timeRun = 0;
+var timer;
+
+//timer function - set to 90 seconds
+function start() {
+
+    timeRun = 90;
+    document.getElementById("timeLeft").innerHTML = timeRun;
+//set interval to subtract time by 1 second
+    timer = setInterval(function() {
+        timeRun--;
+        document.getElementById("timeLeft").innerHTML = timeRun;
+        //if statement to stop game if the timer hits zero
+        if (timeRun <= 0) {
+            clearInterval(timer);
+            stopGame(); 
+        }
+    }, 1000);
+
+    next();
+}
+
+
+
+
+
